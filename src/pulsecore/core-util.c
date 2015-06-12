a171056b7c8191386be8cfa2279343ecef7aa6c4
Deepak Srivastava <srivastava.d@samsung.com>
2015-06-12 18:17:07 +0530

    Rename functions with "tostring" in the name to one with "to_string" to conform with the convention.
    component: core
    
    <EP-E358F00C1D9A449EAE69225B9D2530F8>
    BugLink: https://bugs.freedesktop.org/show_bug.cgi?id=88833
    
    Signed-off-by: Deepak Srivastava <srivastava.d@samsung.com>

--- src/pulsecore/core-util.c
+++ src/pulsecore/core-util.c
@@ -3201,7 +3201,7 @@ char *pa_replace(const char*s, const char*a, const char *b) {
 
     pa_strbuf_puts(sb, s);
 
-    return pa_strbuf_tostring_free(sb);
+    return pa_strbuf_to_string_free(sb);
 }
 
 char *pa_escape(const char *p, const char *chars) {
@@ -3223,7 +3223,7 @@ char *pa_escape(const char *p, const char *chars) {
         pa_strbuf_putc(buf, *s);
     }
 
-    return pa_strbuf_tostring_free(buf);
+    return pa_strbuf_to_string_free(buf);
 }
 
 char *pa_unescape(char *p) {
