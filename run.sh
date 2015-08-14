e554786a7dbd1b227ebfa5a076e8d132d4bad407
Ahmed S. Darwish <darwish.07@gmail.com>
2015-08-15 00:10:51 +0200

    run.sh: Add rationale for .libs/ DL search path suffix removal

--- run.sh
+++ run.sh
@@ -8,6 +8,19 @@ LOG_DIR=build/logs
 HOME_CONFIG_CLIENT_CONF=$HOME/.pulse/client.conf
 AUTOSPAWN_NO=' *autospawn *= *no *'
 
+#
+# Even while PA modules actually exist under `build/src/.libs'
+# instead of just `build/src', specificy the latter as the DL
+# search path.
+#
+# Per the libtool documentation, libtool already adds the
+# .libs/ suffix for us if we're running pulseAudio from within
+# the source repository.
+#
+# It does this by applying some Unix trickery and by making
+# `build/src/pulseaudio' actually a shell script wrapper
+# around the real PA existing at `build/src/.libs/pulseaudio'!
+#
 PA=build/src/pulseaudio
 PA_FLAGS="-n -F build/src/default.pa -p $(pwd)/build/src/ -vvvv"
 
